2ef37953530eda616f6cd43d64dd0385180b18f2
gilremymailgun <gil.remy@mailgun.com>
2026-06-18 08:32:17 -0500

    Updates

--- assets/NavIcons-cmx09qZS.js
+++
@@ -1,8 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{A as t,t as n}from"./iframe-BX-4fR9Z.js";var r=e((()=>{})),i,a,o,s=e((()=>{t(),r(),i=n(),a={dashboard:`home`,reporting:`insert_chart`,send:`send`,inspect:`dashboard`,optimize:`speed`,validate:`mark_email_read`,sms:`chat`,integrations:`integration_instructions`},o=({name:e,className:t})=>(0,i.jsx)(`span`,{className:`material-symbols-rounded nav-icon-font${t?` ${t}`:``}`,"aria-hidden":`true`,children:a[e]}),o.__docgenInfo={description:``,methods:[],displayName:`NavIcon`,props:{name:{required:!0,tsType:{name:`union`,raw:`| 'dashboard'
-| 'reporting'
-| 'send'
-| 'inspect'
-| 'optimize'
-| 'validate'
-| 'sms'
-| 'integrations'`,elements:[{name:`literal`,value:`'dashboard'`},{name:`literal`,value:`'reporting'`},{name:`literal`,value:`'send'`},{name:`literal`,value:`'inspect'`},{name:`literal`,value:`'optimize'`},{name:`literal`,value:`'validate'`},{name:`literal`,value:`'sms'`},{name:`literal`,value:`'integrations'`}]},description:``},className:{required:!1,tsType:{name:`string`},description:``}}}}));export{s as n,o as t};
