f16d97782e54169d45d57640b563d75982f9feee
gilremymailgun <gil.remy@mailgun.com>
2026-07-31 09:27:50 -0500

    Fix Icon import path in Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Button } from './Button';
-import { Icon } from '../icons/Icon';
+import { Icon } from '../../icons/Icon';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
