7842cd056f512fa41b50374ead1bd608b1228bd0
gilremymailgun <gil.remy@mailgun.com>
2026-08-03 11:21:00 -0500

    Updates

---
+++ assets/Label.stories-quW-IJmH.js
@@ -0,0 +1,11 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{A as t,t as n}from"./iframe-Bu2YgMg1.js";import{n as r,t as i}from"./Icon-Cu0Pae4E.js";var a=e((()=>{})),o,s,c=e((()=>{t(),a(),o=n(),s=({children:e,icon:t,className:n})=>(0,o.jsxs)(`div`,{className:[`mg-label`,n||``].filter(Boolean).join(` `),children:[t&&(0,o.jsx)(`span`,{className:`mg-label__icon`,children:t}),(0,o.jsx)(`span`,{className:`mg-label__text`,children:e})]}),s.__docgenInfo={description:``,methods:[],displayName:`Label`,props:{children:{required:!0,tsType:{name:`ReactReactNode`,raw:`React.ReactNode`},description:``},icon:{required:!1,tsType:{name:`ReactReactNode`,raw:`React.ReactNode`},description:`Optional icon element, e.g. <Icon name="..." />`},className:{required:!1,tsType:{name:`string`},description:``}}}})),l,u,d,f,p;e((()=>{c(),r(),l=n(),u={title:`Components/Label`,component:s,tags:[`autodocs`],argTypes:{children:{control:`text`}}},d={args:{children:`Label`}},f={name:`With icon`,args:{children:`Label`,icon:(0,l.jsx)(i,{name:`navigate/check-large-gen2`})}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Label'
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  name: 'With icon',
+  args: {
+    children: 'Label',
+    icon: <Icon name="navigate/check-large-gen2" />
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`WithIcon`]}))();export{d as Default,f as WithIcon,p as __namedExportsOrder,u as default};
