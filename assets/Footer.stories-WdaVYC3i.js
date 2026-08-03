7dce0ab1aae43030421715ecbd67a6ebc761ce45
gilremymailgun <gil.remy@mailgun.com>
2026-08-03 14:20:28 -0500

    Updates

---
+++ assets/Footer.stories-WdaVYC3i.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,t as n}from"./Footer-DV3NNdql.js";var r,i,a;e((()=>{t(),r={title:`Navigation/Footer`,component:n,tags:[`autodocs`]},i={},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
