6a5c356111cb485accbfde82624cf09293ee73c4
gilremymailgun <gil.remy@mailgun.com>
2026-09-18 12:00:41 -0500

    Updates

---
+++ assets/Menu.stories-BVYUlVu4.js
@@ -0,0 +1,66 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{A as t,t as n}from"./iframe-CV2d6n22.js";var r=e((()=>{})),i,a,o,s,c,l,u,d=e((()=>{t(),r(),i=n(),a=({label:e})=>(0,i.jsx)(`div`,{className:`menu-title`,children:e}),o=()=>(0,i.jsx)(`div`,{className:`menu-separator`}),s=({label:e,onClick:t})=>(0,i.jsx)(`div`,{className:`menu-button-wrap`,children:(0,i.jsx)(`button`,{className:`menu-button`,type:`button`,onClick:t,children:e})}),c=({label:e,subLabel:t,disabled:n,leadingIcon:r,trailingIcon:a,badge:o,onClick:s})=>(0,i.jsxs)(`div`,{className:`menu-item${n?` menu-item--disabled`:``}`,onClick:n?void 0:s,role:`menuitem`,"aria-disabled":n,children:[(0,i.jsxs)(`div`,{className:`menu-item__content`,children:[r&&(0,i.jsx)(`div`,{className:`menu-item__leading-icon`,children:r}),(0,i.jsxs)(`div`,{className:`menu-item__text`,children:[(0,i.jsx)(`span`,{className:`menu-item__label`,children:e}),t&&(0,i.jsx)(`span`,{className:`menu-item__sublabel`,children:t})]}),o&&(0,i.jsx)(`span`,{className:`menu-item__badge`,children:o})]}),a&&(0,i.jsx)(`div`,{className:`menu-item__trailing-icon`,children:a})]}),l=()=>(0,i.jsx)(`svg`,{width:`16`,height:`16`,viewBox:`0 0 16 16`,fill:`none`,"aria-hidden":`true`,children:(0,i.jsx)(`path`,{d:`M6 4L10 8L6 12`,stroke:`currentColor`,strokeWidth:`1.5`,strokeLinecap:`round`,strokeLinejoin:`round`})}),u=({items:e,width:t=254})=>(0,i.jsx)(`div`,{className:`menu`,style:{width:t},role:`menu`,children:(0,i.jsx)(`div`,{className:`menu__inner`,children:e.map(e=>e.type===`title`?(0,i.jsx)(a,{label:e.label},e.id):e.type===`separator`?(0,i.jsx)(o,{},e.id):e.type===`button`?(0,i.jsx)(s,{label:e.label,onClick:e.onClick},e.id):(0,i.jsx)(c,{label:e.label,subLabel:e.subLabel,disabled:e.disabled,leadingIcon:e.leadingIcon,trailingIcon:e.trailingIcon??(0,i.jsx)(l,{}),badge:e.badge,onClick:e.onClick},e.id))})}),u.__docgenInfo={description:``,methods:[],displayName:`Menu`,props:{items:{required:!0,tsType:{name:`Array`,elements:[{name:`MenuItem`}],raw:`MenuItem[]`},description:``},width:{required:!1,tsType:{name:`number`},description:``,defaultValue:{value:`254`,computed:!1}}}}})),f,p,m,h,g,_,v,y,b,x;e((()=>{d(),f=n(),p={title:`Components/Menu`,component:u,tags:[`autodocs`],parameters:{layout:`centered`}},m=()=>(0,f.jsx)(`svg`,{width:`16`,height:`16`,viewBox:`0 0 16 16`,fill:`none`,children:(0,f.jsx)(`path`,{fillRule:`evenodd`,clipRule:`evenodd`,d:`M8 1.5C4.41 1.5 1.5 4.41 1.5 8C1.5 11.59 4.41 14.5 8 14.5C11.59 14.5 14.5 11.59 14.5 8C14.5 4.41 11.59 1.5 8 1.5ZM7.5 3.07V5H6C6.43 4.07 6.94 3.46 7.5 3.07ZM8.5 3.07C9.06 3.46 9.57 4.07 10 5H8.5V3.07ZM3.07 9H5.07C5.02 8.67 5 8.34 5 8C5 7.66 5.02 7.33 5.07 7H3.07C2.98 7.32 2.94 7.66 2.94 8C2.94 8.34 2.98 8.68 3.07 9ZM6.07 7C6.02 7.33 6 7.66 6 8C6 8.34 6.02 8.67 6.07 9H9.93C9.98 8.67 10 8.34 10 8C10 7.66 9.98 7.33 9.93 7H6.07ZM10.93 7C10.98 7.33 11 7.66 11 8C11 8.34 10.98 8.67 10.93 9H12.93C13.02 8.68 13.06 8.34 13.06 8C13.06 7.66 13.02 7.32 12.93 7H10.93ZM10 11H8.5V12.93C9.06 12.54 9.57 11.93 10 11ZM7.5 12.93V11H6C6.43 11.93 6.94 12.54 7.5 12.93ZM5.07 9H3.07C3.29 9.97 3.74 10.85 4.36 11.58L5.57 10.37C5.37 9.95 5.19 9.49 5.07 9ZM10.93 9C10.81 9.49 10.63 9.95 10.43 10.37L11.64 11.58C12.26 10.85 12.71 9.97 12.93 9H10.93ZM4.36 4.42L5.57 5.63C5.37 6.05 5.19 6.51 5.07 7H3.07C3.29 6.03 3.74 5.15 4.36 4.42ZM10.43 5.63L11.64 4.42C12.26 5.15 12.71 6.03 12.93 7H10.93C10.81 6.51 10.63 6.05 10.43 5.63Z`,fill:`currentColor`})}),h=()=>(0,f.jsxs)(`svg`,{width:`16`,height:`16`,viewBox:`0 0 16 16`,fill:`none`,children:[(0,f.jsx)(`path`,{fillRule:`evenodd`,clipRule:`evenodd`,d:`M8 5C6.34 5 5 6.34 5 8C5 9.66 6.34 11 8 11C9.66 11 11 9.66 11 8C11 6.34 9.66 5 8 5ZM6 8C6 6.9 6.9 6 8 6C9.1 6 10 6.9 10 8C10 9.1 9.1 10 8 10C6.9 10 6 9.1 6 8Z`,fill:`currentColor`}),(0,f.jsx)(`path`,{fillRule:`evenodd`,clipRule:`evenodd`,d:`M8 1C7.45 1 7 1.45 7 2V2.07C6.48 2.19 5.99 2.39 5.54 2.66L5.49 2.61C5.1 2.22 4.47 2.22 4.08 2.61L2.61 4.08C2.22 4.47 2.22 5.1 2.61 5.49L2.66 5.54C2.39 5.99 2.19 6.48 2.07 7H2C1.45 7 1 7.45 1 8C1 8.55 1.45 9 2 9H2.07C2.19 9.52 2.39 10.01 2.66 10.46L2.61 10.51C2.22 10.9 2.22 11.53 2.61 11.92L4.08 13.39C4.47 13.78 5.1 13.78 5.49 13.39L5.54 13.34C5.99 13.61 6.48 13.81 7 13.93V14C7 14.55 7.45 15 8 15C8.55 15 9 14.55 9 14V13.93C9.52 13.81 10.01 13.61 10.46 13.34L10.51 13.39C10.9 13.78 11.53 13.78 11.92 13.39L13.39 11.92C13.78 11.53 13.78 10.9 13.39 10.51L13.34 10.46C13.61 10.01 13.81 9.52 13.93 9H14C14.55 9 15 8.55 15 8C15 7.45 14.55 7 14 7H13.93C13.81 6.48 13.61 5.99 13.34 5.54L13.39 5.49C13.78 5.1 13.78 4.47 13.39 4.08L11.92 2.61C11.53 2.22 10.9 2.22 10.51 2.61L10.46 2.66C10.01 2.39 9.52 2.19 9 2.07V2C9 1.45 8.55 1 8 1Z`,fill:`currentColor`})]}),g=[{id:`title-1`,label:`SECTION ONE`,type:`title`},{id:`btn-1`,label:`Button`,type:`button`},{id:`item-1`,label:`Label`,subLabel:`SubLabel`,leadingIcon:(0,f.jsx)(m,{})},{id:`item-2`,label:`Label`,subLabel:`SubLabel`,leadingIcon:(0,f.jsx)(h,{})},{id:`item-3`,label:`Label`,subLabel:`SubLabel`,disabled:!0,leadingIcon:(0,f.jsx)(h,{})},{id:`sep-1`,label:``,type:`separator`},{id:`title-2`,label:`SECTION TWO`,type:`title`},{id:`item-4`,label:`Label`,subLabel:`SubLabel`},{id:`item-5`,label:`Label`,subLabel:`SubLabel`},{id:`item-6`,label:`Label`,subLabel:`SubLabel`}],_={args:{items:g}},v={args:{items:[{id:`edit`,label:`Edit`},{id:`duplicate`,label:`Duplicate`},{id:`sep`,label:``,type:`separator`},{id:`delete`,label:`Delete`,disabled:!0}]}},y={args:{items:[{id:`title`,label:`DOMAINS`,type:`title`},{id:`us`,label:`US Region`,subLabel:`app.mailgun.com`,badge:`US`},{id:`eu`,label:`EU Region`,subLabel:`app.eu.mailgun.com`,badge:`EU`},{id:`disabled`,label:`Disabled Region`,disabled:!0}]}},b={args:{items:[{id:`domains`,label:`Domains`,leadingIcon:(0,f.jsx)(m,{})},{id:`settings`,label:`Settings`,leadingIcon:(0,f.jsx)(h,{})},{id:`sep`,label:``,type:`separator`},{id:`disabled`,label:`Archived`,leadingIcon:(0,f.jsx)(m,{}),disabled:!0}]}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    items: DEFAULT_ITEMS
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      id: 'edit',
+      label: 'Edit'
+    }, {
+      id: 'duplicate',
+      label: 'Duplicate'
+    }, {
+      id: 'sep',
+      label: '',
+      type: 'separator'
+    }, {
+      id: 'delete',
+      label: 'Delete',
+      disabled: true
+    }]
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      id: 'title',
+      label: 'DOMAINS',
+      type: 'title'
+    }, {
+      id: 'us',
+      label: 'US Region',
+      subLabel: 'app.mailgun.com',
+      badge: 'US'
+    }, {
+      id: 'eu',
+      label: 'EU Region',
+      subLabel: 'app.eu.mailgun.com',
+      badge: 'EU'
+    }, {
+      id: 'disabled',
+      label: 'Disabled Region',
+      disabled: true
+    }]
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      id: 'domains',
+      label: 'Domains',
+      leadingIcon: <GlobeIcon />
+    }, {
+      id: 'settings',
+      label: 'Settings',
+      leadingIcon: <SettingsIcon />
+    }, {
+      id: 'sep',
+      label: '',
+      type: 'separator'
+    }, {
+      id: 'disabled',
+      label: 'Archived',
+      leadingIcon: <GlobeIcon />,
+      disabled: true
+    }]
+  }
+}`,...b.parameters?.docs?.source}}},x=[`Default`,`SimpleList`,`WithBadges`,`WithIcons`]}))();export{_ as Default,v as SimpleList,y as WithBadges,b as WithIcons,x as __namedExportsOrder,p as default};
